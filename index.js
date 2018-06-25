f1d80b343cadf8782bd0ed03e884bcca6bdee701
Dante Razo <dante@razonet.org>
2018-06-25 15:21:44 -0400

    fixed formatting (beautified code)

--- index.js
+++ index.js
@@ -2,18 +2,18 @@ const button = document.querySelector('button')
 const form = document.querySelector('headerForm')
 const formButton = document.querySelector('formButton')
 
-function changeHeader(text){
+function changeHeader(text) {
     const h = document.querySelector('h1')
     h.textContent = "The first header has been changed!"
 
-    if(text){   // if there's input, change the header to that
+    if (text) {   // if there's input, change the header to that
         h.textContent = text
     } else {    // if there isn't, use the default message
         h.textContent = "The first header has been changed!"
     }
 }
 
-function changeSecondHeader(){
+function changeSecondHeader() {
     const h = document.querySelector('h1.header2')
     h.textContent = "The second header has been changed!"
 }

8b74afd182ec902d5aa6ea3e84f23a09649b29d2
Dante Razo <dante@razonet.org>
2018-06-25 13:56:46 -0400

    Added multiple headers, a form, and buttons to the website

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 const button = document.querySelector('button')
+
+function changeHeader(){
+    const h = document.querySelector('h1')
+    h.textContent = "Hello!"
+}
+
+function changeHeader2(){
+    const h2 = document.querySelector('h1.header2')
+    h2.textContent = "This Header Has Been Changed"
+}
+
+function addHeader(){
+    const h2 = document.querySelector('header2')
+    h2.textContent = "This Header Has Been Changed"
+}

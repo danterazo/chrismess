589d3ccdb7dfbdb03ec416cd21f0b0cf7be3fb05
Dante Razo <dante@razonet.org>
2018-06-25 14:17:52 -0400

    removed unnecessary "add header" button

--- index.js
+++ index.js
@@ -1,19 +1,23 @@
 const button = document.querySelector('button')
 //const form = document.querySelector('form.headerForm')
+const formText = document.querySelector('headerText')
 
 function changeHeader(){
     const h = document.querySelector('h1')
-    h.textContent = "Hello!"
+    h.textContent = "The first header has been changed!"
 }
 
 function changeSecondHeader(){
     const h = document.querySelector('h1.header2')
-    h.textContent = "This Header Has Been Changed"
+    h.textContent = "The second header has been changed!"
 }
 
+
+form.onsubmit = formHeader(formText)
 //form.onsubmit = formHeader()
+document.getElementById("h1").innerHTML="testing";
 
 function formHeader(text){
     const h = document.querySelector('h1')
-    h.textContent = text
+    h.textContent = JSON.stringify(text)
 }

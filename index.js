67f2f288a5e896208e35ef9b6309a3e4a2d62f96
Dante Razo <dante@razonet.org>
2018-06-26 15:21:23 -0400

    Separate addToList() into two functions for "super mega bonus credit"

--- index.js
+++ index.js
@@ -20,14 +20,26 @@ function changeSecondHeader() {
 
 // HW2:
 function addToList() {
-    movieName = document.getElementById('form1').value
-    movieYear = document.getElementById('form2').value
-
-    //document.getElementById('form2').value
-
+    const movieName = document.getElementById('form1').value
     const l = document.getElementById('movieList')
-    const item = document.createElement('li')
-    
-    item.textContent = movieName + " (" + movieYear + ")"
-    l.appendChild(item)
+    const name = document.createElement('span')
+    const br = document.createElement('br')
+
+    name.className = "movieName"
+    name.textContent = movieName
+
+    l.appendChild(name)
+    addYearToList() // second function for "super mega bonus credit"
+    l.appendChild(br)
   }
+
+function addYearToList(){
+    const movieYear = document.getElementById('form2').value
+    const l = document.getElementById('movieList')
+    const year = document.createElement('span')
+
+    year.className = "movieYear"
+    year.textContent = " (" + movieYear + ")"
+
+    l.appendChild(year)
+}

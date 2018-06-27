b0c6a3b1fdbecd30e671a7948f78b4ac7a8c3d64
Dante Razo <dante@razonet.org>
2018-06-26 20:55:48 -0400

    Create global variable for list

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const form = document.querySelector('movieForm')
 const formButton = document.querySelector('formButton')
+const list = document.getElementById('movieList')
 
 // HW1
 function changeHeader(text) {
@@ -21,25 +22,23 @@ function changeSecondHeader() {
 // HW2:
 function addToList() {
     const movieName = document.getElementById('form1').value
-    const l = document.getElementById('movieList')
     const name = document.createElement('span')
     const br = document.createElement('br')
-
+    
     name.className = "movieName"
     name.textContent = movieName
 
-    l.appendChild(name)
+    list.appendChild(name)
     addYearToList() // second function for "super mega bonus credit"
-    l.appendChild(br)
-  }
+    list.appendChild(br)
+}
 
 function addYearToList(){
     const movieYear = document.getElementById('form2').value
-    const l = document.getElementById('movieList')
     const year = document.createElement('span')
 
     year.className = "movieYear"
     year.textContent = " (" + movieYear + ")"
 
-    l.appendChild(year)
+    list.appendChild(year)
 }
